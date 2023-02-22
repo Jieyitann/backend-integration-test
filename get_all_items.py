2a94300aabe1ded4f05a29c2ce10a2cd997c50b9
Tan Jie Yi <74313226+Jieyitann@users.noreply.github.com>
2023-02-22 10:47:44 +0800

    Update get_all_items.py

--- get_all_items.py
+++ get_all_items.py
@@ -11,7 +11,7 @@ database = 'cosmosdb-tcs-devizdb-maindb'
 container = 'frontend'
 
 
-client = CosmosClient(url = url, credential=key)
+client = CosmosClient(endpoint = url, credential=key)
 database = client.get_database_client(database)
 connection = database.get_container_client(container)
 
