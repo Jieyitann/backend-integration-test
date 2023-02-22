8445d3f0ab728717143cc93da430cfe2e64945b0
Tan Jie Yi <74313226+Jieyitann@users.noreply.github.com>
2023-02-22 10:02:51 +0800

    Update get_all_items.py

--- get_all_items.py
+++ get_all_items.py
@@ -3,8 +3,8 @@ from azure.identity import DefaultAzureCredential
 from azure.cosmos import CosmosClient
 
 # Set the endpoint and key for your Cosmos DB account
-COSMOS_ENDPOINT = os.environ.get('COSMOS_ENDPOINT')
-COSMOS_KEY = os.environ.get('COSMOS_KEY')
+ENDPOINT = os.environ.get('COSMOS_ENDPOINT')
+KEY = os.environ.get('COSMOS_KEY')
 
 # Set the database and container you want to use
 DATABASE_ID = 'integration-test-db'
